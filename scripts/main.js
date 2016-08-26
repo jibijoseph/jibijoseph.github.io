903aa0de1dbaed1fc54a94cd73774af697cd7b05
jibijoseph <jibijoseph@gmail.com>
2016-08-26 09:48:06 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,11 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
-var myImage = document.querySelector('img');
-myImage.onclick = function(){
-	var mySrc = myImage.getAttribute('src');
-	if (mySrc === 'images/sap.png'){
-		myImage.setAttribute('src','images/s4hana.jpg');
-	}
-	else {
-		myImage.setAttribute('src','images/sap.png');
-	}
-}
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-if (!localStorage.getItem('name')){ 
-	setUserName();
-	} 
-else { 
-	var storedName = localStorage.getItem('name'); 
-	myHeading.textContent = 'Hello' + storedName; 
-}
+var randomNumber = Math.floor(Math.random() * 100) + 1;
 
-function setUserName(){
-	var myName = prompt('Name');
-	localStorage.setItem('name',myName);
-	myHeading.textContent = 'Hello' + myName;
-}
+var guesses = document.querySelector('.guesses');
+var lastResult = document.querySelector('.lastResult');
+var lowOrHi = document.querySelector('.lowOrHi');
 
-myButton.onclick = function() {
-  setUserName();
-}
+var guessSubmit = document.querySelector('.guessSubmit');
+var guessField = document.querySelector('.guessField');
+
+var guessCount = 1;
+var resetButton;
