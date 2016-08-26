e116fe935add04c681950876eed5c50f75695ae1
jibijoseph <jibijoseph@gmail.com>
2016-08-26 14:14:15 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,82 @@
+var randomNumber = Math.floor(Math.round() * 100) + 1;
 
+var guessField = document.querySelector(".guessField");
+var submitGuess = document.querySelector(".submitGuess");
+
+var resultAll = document.querySelectorAll(".result", p);
+var previousTries = document.querySelector(".previousTries");
+var currentResult = document.querySelector(".currentResult");
+var hint = document.querySelector(".hint");
+var answer = document.querySelector(".answer");
+
+var guessCount = 1;
+var resetButton;
+var win = false;
+
+previousTries.textContent = 'Previous guesses : ';
+
+function checkGuess(){
+  guessValue = Number(guessField.value);
+  
+  previousTries.textContent += guessValue + ' ';
+  
+  if (guessValue === randomNumber){
+    gameWin();
+  } else if (guessCount === 10 ) {
+    gameOver();
+  } else {
+    gameContinue(guessValue-randomNumber);
+  }
+  
+}
+
+submitGuess.addEventListener('click',checkGuess);
+
+function gameWin(){
+  currentResult.textContent = '!!!Congratulations!!!';
+  currentResult.style.backgroundColor = "green";
+  setGameOver();
+}
+
+function gameOver(){
+  currentResult.textContent = '!!!Game Over!!!';
+  currentResult.style.backgroundColor = "red";
+  answer.textContent = 'The answer was ' + randomNumber;
+  setGameOver();
+}
+
+function gameContinue(hintValue){
+  currentResult.textContent = 'Wrong guess. Try again';
+  currentResult.style.backgroundColor = "yellow";
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+}
+
+function gameOver(){
+  guessField.disabled = true;
+  submitGuess.disabled = true;
+
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Start over';
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener('click',resetGame);
+}
+
+function resetGame(){
+  guessField.disabled = false;
+  submitGuess.disabled = false;
+  guessField.value = '';
+  guessField.setFocus();
+  
+  for (i=0; i<resultAll.length; i++){
+    resultAll[i].textContent = '';
+  }
+  
+  previousTries.textContent = 'Previous guesses : ';
+  
+  resetButton.parentNode.removeChild(resetChild);
+  
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+  
+}
