4e6c6552d0ccebcbeafe695db9e4e36367176ca1
jibijoseph <jibijoseph@gmail.com>
2016-08-26 13:19:17 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,48 @@
-var randomNumber = Math.floor(Math.random() * 100 ) + 1;
+var randomNumber = Math.floor( Math.random() * 100 ) + 1;
 
-var guesses = document.querySelector('.guesses');
-var lastResult = document.querySelector('.lastResult');
-var lowOrHi = document.querySelector('.lowOrHi');
+var guessTries = document.querySelector(".guessTries");
+var guessResult = document.querySelector(".guessResult");
+var answer = document.querySelector(".answer");
+var hint = document.querySelector(".hint");
 
-var guessSubmit = document.querySelector('.guessSubmit');
-var guessField = document.querySelector('.guessField');
+var guessSubmit = document.querySelector(".guessSubmit");
+var guessField = document.querySelector(".gueussField");
+
 var guessCount = 1;
 var resetButton;
-
-guessField.focus();
+var win = false;
 
 function checkGuess(){
   var userGuess = Number(guessField.value);
-  if (guessCount === 1 ){
-    guesses.textContent = 'Previous Guesses: ';
-  }
-  guesses.textContent += userGuess + ' ';
-
-  if (userGuess===randomNumber) {
-    lastResult.textContent = 'Congratulations! You got it right';
-    lastResult.style.backgroundColor = 'green';
-    lowOrHi.textContent = '';
-    setGameOver();
-  } else if (guessCount === 10){
-    lastResult.textContent = '!!! GAME OVER!!!';
-    lastResult.style.backgroundColor = 'red';
-    setGameOver();
-  } else {
-    lastResult.textContent = 'Wrong!';
-    if (userGuess < randomNumber){
-      lowOrHi.textContent = 'Last guess was too low';
-    } else {
-      lowOrHi.textContent = 'Last guess was too high';
-    }  
+  
+  if (guessCount === 1) {
+    guessTries.textContent = 'Previous Guesses: ';
   }
   
-  guessCount++;
-  guessField.value='';
+  guessTries += userGuess + ' ';
+  
+  if (userGuess === randomNumber){
+    guessResult.textConent = "Congratulations you win!!!";
+    guessResult.style.backgroundColor = 'green';
+    win = true.
+    setGameOver();
+  } else {
+    guessResult.textContent = "Wrong!";
+    guessResult.style.backgroundColor = 'red';
+    if (userGuess < randomNumber ){
+      hint.textContent = 'Too low';
+    }
+    else if (userGuess > randomNumber ){
+      hint.textContent = 'Too high';
+    }
+  }
+  
+  if (guessTries === 10) {
+    setGameOver();
+  }
+  
+  guessTries++;
+  guessField.value = '';
   guessField.focus();
 }
 
@@ -46,17 +51,28 @@ guessSubmit.addEventListener('click',checkGuess);
 function setGameOver(){
   guessField.disabled = true;
   guessSubmit.disabled = true;
+  if win = false {
+    answer.textContent = 'The right answer was ' + randomNumber;
+  }
   resetButton = document.createElement('button');
-  resetButton.textContent = 'Start new Game';
-  document.body.appendChild('resetButton');
+  resetButton.textContent = 'Start new game';
+  document.body.appendChild(resetButton);
   resetButton.addEventListener('click',resetGame);
 }
 
 function resetGame(){
   guessCount = 1;
-  var resetParas = document.querySelectorAll('.resultParas p');
-  for (var i = 0; i < resetParas.length; i++) {
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  
+  var resetParas = document.querySelectorAll('.resultParas', p);
+  for (var i=0; i<resetParas.length(); i++ ){
     resetParas[i].textContent = '';
   }
+  
   resetButton.parentNode.removeChild(resetButton);
+  
+  result.style.backgroundColor = 'white';
+  guessField.focus();
 }
